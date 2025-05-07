161ddf9d44edc99a1c11f064396da89fad3eeddd
brianrego03 <brian.rego03@gmail.com>
2025-05-07 17:42:40 +0530

    Connect movie page route to app js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const path=require("node:path");
 const indexRouter=require("./routes/indexRouter");
+const movieRouter=require("./routes/movieRouter");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -9,6 +10,8 @@ app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 
 app.use("/",indexRouter);
+app.use("/movies",movieRouter);
+
 
 const PORT=3000;
 app.listen(PORT,()=>{
