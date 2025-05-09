a51801f20e377bf8af5276f79114ae6c5a5d1a19
brianrego03 <brian.rego03@gmail.com>
2025-05-09 16:10:58 +0530

    Create form to Add a movie

---
+++ public/scripts.js
@@ -0,0 +1,15 @@
+// const createMovieDirectorChange=()=>{
+//     const dropdown = document.getElementById("createMovieDirector");
+//     const selectedValue = dropdown.value;
+
+// }
+
+const displayForm=()=>{
+    document.getElementById("formContainer").style.display="flex";
+    console.log(document.getElementById("formContainer"));
+}
+
+const closeCreateMovie=()=>{
+    document.getElementById("formContainer").style.display="none";
+
+}
