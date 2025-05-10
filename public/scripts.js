7e5e4751eeb908933395904da24c85527c4df7c8
brianrego03 <brian.rego03@gmail.com>
2025-05-10 11:43:53 +0530

    Write a function to append directors after selecting from drop down menu

--- public/scripts.js
+++ public/scripts.js
@@ -13,3 +13,37 @@ const closeCreateMovie=()=>{
     document.getElementById("formContainer").style.display="none";
 
 }
+
+const addDirector=(directorID)=>{
+    const directorSelector=document.getElementById("createMovieDirector");
+    console.log(directorSelector.getAttribute("data-people"));
+    const people=JSON.parse(directorSelector.getAttribute("data-people"));
+    const directorName=people.find(person=>person.id===(+directorID)).name;
+    console.log(directorName);
+    let directorDiv=document.createElement("div");
+
+    let displayDirectorDiv=document.createElement("div");
+    let directorNameDiv=document.createElement("div");
+    directorNameDiv.innerText=directorName;
+    directorNameDiv.style.display="inline-block";
+
+    let deleteDirector=document.createElement("button");
+    deleteDirector.innerText="X";
+    deleteDirector.onclick=()=>{
+        directorDiv.parentNode.removeChild(directorDiv);
+    }
+
+    displayDirectorDiv.appendChild(directorNameDiv);
+    displayDirectorDiv.appendChild(deleteDirector);
+    directorDiv.appendChild(displayDirectorDiv);
+
+    let directorInput=document.createElement("input");
+    directorInput.type="hidden";
+    directorInput.value=+(directorID);
+    directorInput.setAttribute("name","movieDirector[]");
+    directorDiv.appendChild(directorInput);
+
+    const selectedDirector=document.getElementById("selectedDirector");
+    selectedDirector.appendChild(directorDiv);
+
+}
